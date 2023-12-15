5e5594a8d24ccb9e60018ec73bd50fb50ca91449
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-15 14:39:15 +0300

    ..

---
+++ PalindromeLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PalindromeLinkedList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+    
+  public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+ 
+    
+    public class Solution {
+        public bool IsPalindrome(ListNode head) {
+        
+        }
+    }
+}
