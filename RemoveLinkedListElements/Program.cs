3a2c622deafcafffd547414967e3355abcace4a1
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-15 16:10:37 +0300

    ..

---
+++ RemoveLinkedListElements/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace RemoveLinkedListElements
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public class Solution
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+        }
+    }
+}
