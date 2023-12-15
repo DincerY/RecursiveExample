ebbacb699c4b295f33d047b3b76954953a864446
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-15 15:28:23 +0300

    ..

--- PalindromeLinkedList/Program.cs
+++ PalindromeLinkedList/Program.cs
@@ -1,25 +1,79 @@
-﻿namespace PalindromeLinkedList
+﻿using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace PalindromeLinkedList
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            ListNode listNode = new ListNode(1);
+            listNode.next = new ListNode(2);
+            listNode.next.next = new ListNode(2);
+            listNode.next.next.next = new ListNode(1);
+
+            Solution solution = new();
+            solution.IsPalindrome(listNode);
+
         }
     }
-    
-  public class ListNode {
-      public int val;
-      public ListNode next;
-      public ListNode(int val=0, ListNode next=null) {
-          this.val = val;
-          this.next = next;
-      }
-  }
- 
-    
-    public class Solution {
-        public bool IsPalindrome(ListNode head) {
-        
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public class Solution
+    {
+        string stringVal = "";
+        string reverseVal = "";
+
+        public bool IsPalindrome(ListNode head)
+        {
+            ListNodeValueToString(head);
+
+            void Recursion(ListNode head)
+            {
+                head = head.next;
+                if (head != null)
+                {
+                    Recursion(head);
+                }
+
+                reverseVal += head?.val;
+
+            }
+
+            Recursion(head);
+            reverseVal += head.val;
+
+            if (stringVal == reverseVal)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+
+        private string ListNodeValueToString(ListNode head)
+        {
+            while (head != null)
+            {
+                stringVal += head.val;
+                head = head.next;
+            }
+
+            return stringVal;
         }
     }
 }
---
+++ PowerOfThree/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace PowerOfThree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            solution.IsPowerOfThreeRecursion(0);
+        }
+    }
+
+
+    public class Solution
+    {
+        public bool IsPowerOfThreeRecursion(int n)
+        {
+            if (n == 1)
+            {
+                return true;
+            }
+            if (n % 3 != 0 || n == 0)
+            {
+                return false;
+            }
+
+            return IsPowerOfThreeRecursion(n / 3);
+        }
+
+        public bool IsPowerOfThreeWhile(int n)
+        {
+            
+        }
+    }
+}
