4067eeb518f568b85bb6b9ad2c5748e47e3324e7
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-15 16:08:43 +0300

    ..

---
+++ FibonacciNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace FibonacciNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            var a =solution.Fib(6);
+        }
+    }
+
+
+    public class Solution
+    {
+        private Dictionary<int, int> fibCache = new Dictionary<int, int>();
+
+        public int Fib(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n == 0)
+            {
+                return 0;
+            }
+            
+            if (fibCache.ContainsKey(n))
+            {
+                return fibCache[n];
+            }
+
+            fibCache[n] = Fib(n - 1) 
+                          + 
+                          Fib(n - 2);
+            return fibCache[n];
+        }
+    }
+}
