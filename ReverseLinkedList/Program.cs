41a43c3eb449bef81dafba471f28e603831ee9b2
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-20 19:35:57 +0300

    ..

---
+++ ReverseLinkedList/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ReverseLinkedList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+            
+            
+            Solution solution = new();
+            solution.ReverseList(head);
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode dummyNode = new();
+            void Recursion(ListNode node)
+            {
+                
+            }
+            
+        }
+    }
+}
