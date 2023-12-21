82a848d1606a2f30879add99b5a1c3af929b3380
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-21 17:16:04 +0300

    ..

--- PowerOfNumber/Program.cs
+++ PowerOfNumber/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace PowerOfNumber
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PowerOfNumber
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Solution solution = new();
+            var a = solution.MyPow(2.00000, -2);
+        }
+    }
+
+    public class Solution
+    {
+        public double MyPow(double x, int n)
+        {
+            double res = 0;
+
+            double Recursion(int newN)
+            {
+                if (newN == 0)
+                {
+                    return 1;
+                }
+
+                res = Recursion(newN / 2);
+                res = res * res;
+                if (newN % 2 == 0)
+                {
+                    return res;
+                }
+                else
+                {
+                    return res * x;
+                }
+            }
+
+            return n < 0 ? 1 / Recursion(n) : Recursion(n);
         }
     }
 }
