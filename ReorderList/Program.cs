0aa9a253f99606c6558baa9b78662bc44f0d1306
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-22 13:26:10 +0300

    ..

--- ReorderList/Program.cs
+++ ReorderList/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace ReorderList
+﻿using System;
+
+namespace ReorderList
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            ListNode head = new(1, new(2, new(3, new(4, new(5, new(6, new(7, new(8))))))));
+            ListNode head1 = new ListNode(1);
+
+
+            Solution solution = new();
+            solution.ReorderList(head);
         }
     }
 
@@ -24,6 +32,36 @@
     {
         public void ReorderList(ListNode head)
         {
+            ListNode Recursion(ListNode node)
+            {
+                ListNode prev = null;
+                if (node.next == null)
+                {
+                    return node;
+                }
+                if (node.next.next == null)
+                {
+                    var a = node.next;
+                    node.next = null;
+                    prev = head.next;
+                    head.next = a;
+                    a.next = prev;
+                    return prev;
+                }
+                ListNode pr = Recursion(node.next);
+                if (pr?.next == null)
+                {
+                    return pr;
+                }
+                ListNode prr = null;
+                ListNode b = node.next;
+                node.next = null;
+                prr = pr.next;
+                pr.next = b;
+                b.next = prr;
+                return prr;
+            }
+            Recursion(head);
         }
     }
 }
