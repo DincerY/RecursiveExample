ff42697db9726a47bd4f4d099bcd25e2bab981b9
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-22 20:11:13 +0300

    ..

--- DecodeString/Program.cs
+++ DecodeString/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace DecodeString
 {
@@ -8,16 +11,54 @@ namespace DecodeString
         public static void Main(string[] args)
         {
             Solution solution = new();
-            solution.DecodeString("3[a]2[bc]");
+            //   3[a]2[bc]
+            var a = solution.DecodeString("3[a2[c]]");
+            Console.WriteLine(a);
         }
     }
-    
-    
-    public class Solution {
+
+
+    public class Solution
+    {
         public string DecodeString(string s)
         {
-            Stack<string> stack = new(); 
+            Stack<string> stack = new();
+            for (int i = 0; i < s.Length; i++)
+            {
+                string a = "";
+                string power = "";
+                int j = i;
+                if (s[i] != ']')
+                {
+                    stack.Push(s[i].ToString());
+                }
+                else
+                {
+                    
+                    while (s[j] != '[')
+                    {
+                        if (stack.Peek() == "[")
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            a += stack.Pop();
+                        }
 
+                        j--;
+                    }
+
+                    
+                    while (j > 0)
+                    {
+                        power += stack.Pop();
+                        j--;
+                    }
+                    
+                    stack.Push(string.Concat(Enumerable.Repeat(a, int.Parse(power))));
+                }
+            }
 
 
             return "";
---
+++ NumberOfDigitOne/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfDigitOne
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            var a = solution.CountDigitOneWithFor(824883294);
+            Console.WriteLine(a);
+        }
+    }
+    
+    
+    public class Solution {
+
+        public int CountDigitOne(int n)
+        {
+
+
+
+
+            return 0;
+        }
+
+        
+        public int CountDigitOneWithFor(int n)
+        {
+            //Add memoization
+            Dictionary<string, int> cache = new();  
+            int total = 0;
+            void Recursion(int a)
+            {
+                if (a == 0)
+                {
+                    return;
+                }
+
+                if (a%10 == 1)
+                {
+                    total++;
+                }
+                Recursion(a/10);
+            }
+            
+            for (int i = n; i > 0; i--)
+            {
+                Recursion(i);
+            }
+            
+            return total;
+        }
+    }
+}
