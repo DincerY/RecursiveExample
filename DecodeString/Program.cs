543be7ea8ea59ad7cada8f3919194d0ef13bf1b8
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-22 13:30:13 +0300

    ..

---
+++ DecodeString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DecodeString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            solution.DecodeString("3[a]2[bc]");
+        }
+    }
+    
+    
+    public class Solution {
+        public string DecodeString(string s)
+        {
+            Stack<string> stack = new(); 
+
+
+
+            return "";
+        }
+    }
+}
