4484d3a2016dd8051362795800648b70909e1b03
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-01-02 16:43:59 +0300

    ..

---
+++ AllPossibleFullBinaryTrees/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Solution solution = new();
+solution.AllPossibleFBT(7);
+
+Console.WriteLine("Hello, World!");
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<TreeNode> AllPossibleFBT(int n)
+    {
+        List<TreeNode> result = new();
+        TreeNode head = new TreeNode();
+        int total = 1;
+
+        void Recusion(TreeNode node)
+        {
+            if (total + 2 <= n)
+            {
+                total += 2;
+                node.left = new();
+                node.right = new();
+                //right
+                Recusion(node.right);
+                result.Add(node);
+                //left
+                Recusion(node.left);
+                result.Add(node);
+            }
+        }
+        Recusion(head);
+        return result;
+    }
+}
--- NumberOfDigitOne/Program.cs
+++ NumberOfDigitOne/Program.cs
@@ -8,7 +8,7 @@ namespace NumberOfDigitOne
         public static void Main(string[] args)
         {
             Solution solution = new();
-            var a = solution.CountDigitOneWithFor(824883294);
+            var a = solution.CountDigitOne(11);
             Console.WriteLine(a);
         }
     }
@@ -18,39 +18,12 @@ namespace NumberOfDigitOne
 
         public int CountDigitOne(int n)
         {
-
+            
 
 
 
             return 0;
         }
-
         
-        public int CountDigitOneWithFor(int n)
-        {
-            //Add memoization
-            Dictionary<string, int> cache = new();  
-            int total = 0;
-            void Recursion(int a)
-            {
-                if (a == 0)
-                {
-                    return;
-                }
-
-                if (a%10 == 1)
-                {
-                    total++;
-                }
-                Recursion(a/10);
-            }
-            
-            for (int i = n; i > 0; i--)
-            {
-                Recursion(i);
-            }
-            
-            return total;
-        }
     }
 }
